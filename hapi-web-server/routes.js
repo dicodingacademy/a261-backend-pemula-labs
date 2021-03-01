c472ec8816f7c51d09ecaa6fdafa885a40f941a8
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 17:28:20 +0700

    routing

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,39 @@
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Homepage';
+    },
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method';
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return 'Halaman tidak ditemukan';
+    },
+  },
+];
+
+module.exports = routes;
