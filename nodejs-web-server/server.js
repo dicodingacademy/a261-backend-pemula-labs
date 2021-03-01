6197df8dc58b0945a489fee9693d8be042377690
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 16:57:14 +0700

    Handling request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,9 +2,26 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-
   response.statusCode = 200;
-  response.end('<h1>Halo HTTP Server!</h1>');
+
+  const { method } = request;
+
+  if(method === 'GET') {
+    response.end('<h1>Hello!</h1>');
+  }
+
+  if(method === 'POST') {
+    response.end('<h1>Hai!</h2>');
+  }
+
+  if(method === 'PUT') {
+    response.end('<h1>Bonjour!</h1>');
+  }
+
+  if(method === 'DELETE') {
+    response.end('<h1>Salam!</h1>');
+  }
+
 };
 
 const server = http.createServer(requestListener);
