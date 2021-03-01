fd782e86ced49917c0eba29ab3ce6abb58b1afdf
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 17:12:25 +0700

    routing request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,24 +4,34 @@ const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
 
-  if(method === 'GET') {
-    response.end('<h1>Hello!</h1>');
-  }
+  if(url === '/') {
+    if(method === 'GET') {
+      response.end('<h1>Ini adalah homepage</h1>');
+    } else {
+      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+    }
+  } else if(url === '/about') {
+    if(method === 'GET') {
+      response.end('<h1>Halo! Ini adalah halaman about</h1>')
+    } else if(method === 'POST') {
+      let body = [];
 
-  if(method === 'POST') {
-    let body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
 
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    });
-
-    request.on('end', () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h2>`);
-    });
+      request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+    }
+  } else {
+    response.end('<h1>Halaman tidak ditemukan!</h1>');
   }
 };
 
