fc45c454e73921f2e50dab25d35236872bfe1eae
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 16:55:07 +0700

    membuat HTTP server

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,1 +1,17 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+
+  response.statusCode = 200;
+  response.end('<h1>Halo HTTP Server!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
