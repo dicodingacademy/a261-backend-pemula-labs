6033cbae242ce6da6cc1d102a6d7042156dc4bb7
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 17:32:23 +0700

    Query Parameters

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -32,6 +32,12 @@ const routes = [
     path: '/hello/{name?}',
     handler: (request, h) => {
       const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if(lang === 'id') {
+        return `Hai, ${name}!`;
+      }
+
       return `Hello, ${name}!`;
     },
   },
