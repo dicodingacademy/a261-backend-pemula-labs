72beb1f9942cf57965d8b1f27f9a3a3721353221
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 17:30:43 +0700

    Path parameter

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -28,6 +28,14 @@ const routes = [
     },
   },
   {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      return `Hello, ${name}!`;
+    },
+  },
+  {
     method: '*',
     path: '/{any*}',
     handler: (request, h) => {
