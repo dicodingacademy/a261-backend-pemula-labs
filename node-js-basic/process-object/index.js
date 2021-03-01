c2fdbfa10fa66c99e59da6480d04e3c9ba1836e1
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 14:36:33 +0700

    process object practice solution

---
+++ node-js-basic/process-object/index.js
@@ -0,0 +1,13 @@
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for(let i = 0; i <= 10000; i++) {
+  // Proses looping ini akan membuat penggunaan memori naik
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
