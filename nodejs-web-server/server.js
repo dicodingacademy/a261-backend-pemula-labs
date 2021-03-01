2c4da4a8d84752dae04651cfa823abedf5ecb027
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 17:16:17 +0700

    mengubah response status code

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,18 +2,20 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-  response.statusCode = 200;
 
   const { method, url } = request;
 
   if(url === '/') {
     if(method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>Ini adalah homepage</h1>');
     } else {
+      response.statusCode = 400;
       response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
   } else if(url === '/about') {
     if(method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>Halo! Ini adalah halaman about</h1>')
     } else if(method === 'POST') {
       let body = [];
@@ -25,12 +27,15 @@ const requestListener = (request, response) => {
       request.on('end', () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        response.statusCode = 200;
         response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
     }
   } else {
+    response.statusCode = 404;
     response.end('<h1>Halaman tidak ditemukan!</h1>');
   }
 };
