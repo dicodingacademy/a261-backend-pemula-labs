996e9aa1142884450d7ffb8f9f4adb1905a800c8
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-01 17:17:18 +0700

    mengubah nilai header response

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-  response.setHeader('Content-Type', 'text/html');
+  response.setHeader('Content-Type', 'application/json');
+  response.setHeader('X-Powered-By', 'NodeJS');
 
   const { method, url } = request;
 
