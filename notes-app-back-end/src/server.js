7023f8c8f8dd9ec3fbe733a5c2cc657e7b4cfd80
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-02 09:31:52 +0700

    membuat HTTP server

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,1 +1,13 @@
-console.log('Halo kita akan membuat RESTful API');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
