a9c52d0d62406f1736f3baad84ac0fa83179dd49
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-03-02 09:44:45 +0700

    menerapkan CORS pada web server

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
