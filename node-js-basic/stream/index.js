77dcb326c1f85daec5895dc806a50c9a42915b52
Arsy Opraza Akma <34035232+arasopraza@users.noreply.github.com>
2025-06-20 16:57:48 +0700

    refactor: change commonjs to esm

--- node-js-basic/stream/index.js
+++ node-js-basic/stream/index.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const { resolve } = require('path');
+import fs from 'fs';
+import { resolve } from 'path';
 
-const readableStream = fs.createReadStream(resolve(__dirname, 'input.txt'), {
+const readableStream = fs.createReadStream(resolve('data.txt'), {
   highWaterMark: 15,
 });
 
-const writableStream = fs.createWriteStream(resolve(__dirname, 'output.txt'));
+const writableStream = fs.createWriteStream(resolve('output.txt'));
 
 readableStream.on('readable', () => {
   try {
