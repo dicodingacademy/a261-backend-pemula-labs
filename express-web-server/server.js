f560e9cd7e069d7a40c6c4498f8319ef9c0852cb
arasopraza <aras.opraza@gmail.com>
2025-09-21 14:27:12 +0700

    refactor: add host

--- express-web-server/server.js
+++ express-web-server/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import routes from './routes.js';
 
 const app = express();
+const host = 'localhost'
 const port = 3000;
 
 app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
